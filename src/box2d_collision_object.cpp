cb89f36700cec0090933c2c0da5e77dbf7001b2f
Ricardo Buring <ricardo.buring@gmail.com>
2022-09-08 22:18:19 +0200

    Box2DCollisionObject: add shapes (first try)

--- src/box2d_collision_object.cpp
+++ src/box2d_collision_object.cpp
@@ -2,15 +2,102 @@
 
 #include <godot_cpp/core/memory.hpp>
 
+#include <box2d/b2_fixture.h>
+
+void Box2DCollisionObject::_update_shapes() {
+	if (!space) {
+		return;
+	}
+
+	for (int i = 0; i < shapes.size(); i++) {
+		Shape &s = shapes.write[i];
+		if (s.disabled) {
+			continue;
+		}
+
+		//not quite correct, should compute the next matrix..
+		Transform2D xform = transform * s.xform;
+
+		if (!s.fixture) {
+			b2FixtureDef fixture_def;
+			fixture_def.shape = s.shape->get_b2Shape();
+			// TODO: use xform here
+			fixture_def.density = 1.0f;
+			s.fixture = body->CreateFixture(&fixture_def);
+		}
+		// TODO: use i?
+
+		//space->get_broadphase()->move(s.bpid, shape_aabb);
+	}
+}
+
+void Box2DCollisionObject::add_shape(Box2DShape *p_shape, const Transform2D &p_transform, bool p_disabled) {
+	Shape s;
+	s.shape = p_shape;
+	s.xform = p_transform;
+	s.disabled = p_disabled;
+	shapes.push_back(s);
+
+	// TODO (queue) update
+}
+
+void Box2DCollisionObject::set_shape(int p_index, Box2DShape *p_shape) {
+	ERR_FAIL_INDEX(p_index, shapes.size());
+	//shapes[p_index].shape->remove_owner(this);
+	shapes.write[p_index].shape = p_shape;
+
+	// TODO: (queue) update
+}
+
+void Box2DCollisionObject::set_shape_transform(int p_index, const Transform2D &p_transform) {
+	ERR_FAIL_INDEX(p_index, shapes.size());
+
+	shapes.write[p_index].xform = p_transform;
+
+	// TODO: (queue) update
+}
+
+void Box2DCollisionObject::remove_shape(Box2DShape *p_shape) {
+	//remove a shape, all the times it appears
+	for (int i = 0; i < shapes.size(); i++) {
+		if (shapes[i].shape == p_shape) {
+			remove_shape(i);
+			i--;
+		}
+	}
+}
+
+void Box2DCollisionObject::remove_shape(int p_index) {
+	//remove anything from shape to be erased to end, so subindices don't change
+	ERR_FAIL_INDEX(p_index, shapes.size());
+	for (int i = p_index; i < shapes.size(); i++) {
+		if (!shapes[i].fixture) {
+			continue;
+		}
+		//should never get here with a null owner
+		body->DestroyFixture(shapes[i].fixture);
+		shapes.write[i].fixture = nullptr;
+	}
+	shapes.remove_at(p_index);
+
+	// TODO: (queue) update
+}
+
 void Box2DCollisionObject::_set_space(Box2DSpace* p_space) {
 	if (space) {
+		for (int i = 0; i < shapes.size(); i++) {
+			Shape &s = shapes.write[i];
+			if (s.fixture) {
+				body->DestroyFixture(shapes[i].fixture);
+				s.fixture = nullptr;
+			}
+		}
 		space->remove_object(this);
-		// TODO: remove shapes?
 	}
 	space = p_space;
 	if (space) {
 		space->add_object(this);
-		// TODO: update shapes?
+		_update_shapes();
 	}
 }
 
--- src/box2d_collision_object.h
+++ src/box2d_collision_object.h
@@ -3,9 +3,11 @@
 
 #include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/variant/rid.hpp>
+#include <godot_cpp/templates/vector.hpp>
 
 #include <box2d/b2_body.h>
 
+#include "box2d_shape.h"
 #include "box2d_space.h"
 
 using namespace godot;
@@ -17,6 +19,18 @@ private:
 	b2Body *body = nullptr;
 	Box2DSpace* space = nullptr;
 
+	struct Shape {
+		Transform2D xform;
+		Box2DShape *shape = nullptr;
+		b2Fixture *fixture = nullptr;
+		bool disabled = false;
+	};
+
+	Vector<Shape> shapes;
+	Transform2D transform;
+
+	void _update_shapes();
+
 protected:
 	void _set_space(Box2DSpace* p_space);
 
@@ -26,6 +40,24 @@ public:
 
 	b2Body *get_b2Body() { return body; }
 	void set_b2Body(b2Body* p_body) { body = p_body; }
+
+	void add_shape(Box2DShape *p_shape, const Transform2D &p_transform = Transform2D(), bool p_disabled = false);
+	void set_shape(int p_index, Box2DShape *p_shape);
+	void set_shape_transform(int p_index, const Transform2D &p_transform);
+
+	_FORCE_INLINE_ int get_shape_count() const { return shapes.size(); }
+	_FORCE_INLINE_ Box2DShape *get_shape(int p_index) const {
+		CRASH_BAD_INDEX(p_index, shapes.size());
+		return shapes[p_index].shape;
+	}
+
+	_FORCE_INLINE_ const Transform2D &get_shape_transform(int p_index) const {
+		CRASH_BAD_INDEX(p_index, shapes.size());
+		return shapes[p_index].xform;
+	}
+
+	void remove_shape(Box2DShape *p_shape);
+	void remove_shape(int p_index);
 
 	virtual void set_space(Box2DSpace* p_space) = 0;
 	_FORCE_INLINE_ Box2DSpace *get_space() const { return space; }
