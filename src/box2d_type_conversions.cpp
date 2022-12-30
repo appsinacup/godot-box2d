5c84ae51205356ec819c8c77359974e8672318f0
Ricardo Buring <ricardo.buring@gmail.com>
2022-12-30 18:25:16 +0100

    Extend type conversions to both single and double precision
    Also clean up parameter names in type conversions.

--- src/box2d_type_conversions.cpp
+++ src/box2d_type_conversions.cpp
@@ -1,19 +1,27 @@
 #include "box2d_type_conversions.h"
 
-void godot_to_box2d(const real_t &x, float &box2d_x) {
-	box2d_x = (float)(x * G_TO_B_FACTOR);
+void godot_to_box2d(const float &p_value, float &r_box2d_value) {
+	r_box2d_value = p_value * G_TO_B_FACTOR;
 }
 
-void godot_to_box2d(const Vector2 &pos, b2Vec2 &box2d_pos) {
-	box2d_pos.x = pos.x * G_TO_B_FACTOR;
-	box2d_pos.y = pos.y * G_TO_B_FACTOR;
+void godot_to_box2d(const double &p_value, float &r_box2d_value) {
+	r_box2d_value = (float)(p_value * G_TO_B_FACTOR);
 }
 
-void box2d_to_godot(const float &box2d_x, real_t &x) {
-	x = (real_t)(B_TO_G_FACTOR * box2d_x);
+void godot_to_box2d(const Vector2 &p_vector, b2Vec2 &r_box2d_vector) {
+	r_box2d_vector.x = p_vector.x * G_TO_B_FACTOR;
+	r_box2d_vector.y = p_vector.y * G_TO_B_FACTOR;
 }
 
-void box2d_to_godot(const b2Vec2 &box2d_pos, Vector2 &pos) {
-	pos.x = B_TO_G_FACTOR * box2d_pos.x;
-	pos.y = B_TO_G_FACTOR * box2d_pos.y;
+void box2d_to_godot(const float &p_box2d_value, float &r_value) {
+	r_value = B_TO_G_FACTOR * p_box2d_value;
+}
+
+void box2d_to_godot(const float &p_box2d_value, double &r_value) {
+	r_value = (double)(B_TO_G_FACTOR * p_box2d_value);
+}
+
+void box2d_to_godot(const b2Vec2 &p_box2d_vector, Vector2 &r_vector) {
+	r_vector.x = B_TO_G_FACTOR * p_box2d_vector.x;
+	r_vector.y = B_TO_G_FACTOR * p_box2d_vector.y;
 }
--- src/box2d_type_conversions.h
+++ src/box2d_type_conversions.h
@@ -10,10 +10,12 @@ using namespace godot;
 #define B_TO_G_FACTOR 50.0
 #define G_TO_B_FACTOR 1.0/50.0
 
-void godot_to_box2d(const real_t &x, float &box2d_x);
-void godot_to_box2d(const Vector2 &pos, b2Vec2 &box2d_pos);
+void godot_to_box2d(const float &p_value, float &r_box2d_value);
+void godot_to_box2d(const double &p_value, float &r_box2d_value);
+void godot_to_box2d(const Vector2 &p_vector, b2Vec2 &r_box2d_vector);
 
-void box2d_to_godot(const float &box2d_x, real_t &x);
-void box2d_to_godot(const b2Vec2 &box2d_pos, Vector2 &pos);
+void box2d_to_godot(const float &p_box2d_value, float &r_value);
+void box2d_to_godot(const float &p_box2d_value, double &r_value);
+void box2d_to_godot(const b2Vec2 &p_box2d_vector, Vector2 &r_vector);
 
 #endif // BOX2D_TYPE_CONVERSIONS_H
