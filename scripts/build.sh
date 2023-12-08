4fca8553ffef7e7988d8ad20670fb76d7da97a3e
Dragos Daian <daian.dragos@yahoo.com>
2023-12-08 22:51:32 +0100

    Try to do releases automatically again (#79)
    * Update release.yml
    
    * use godot cpp 4.2
    
    * Update release.yml
    
    * Update runner.yml
    
    * Update release.yml
    
    * Update action.yml
    
    * Update release.yml
    
    * update
    
    * Update release.yml
    
    * put version in plugin.cfg
    
    * Update action.yml
    
    * only do release builds
    
    * fix collide function

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
-scons target=template_debug generate_bindings=no arch=arm64
+scons target=template_release generate_bindings=no arch=arm64
 rm -rf Godot-Physics-Tests/addons
 cp -rf bin/addons Godot-Physics-Tests/addons
 
